5a638678ccf215969e3dc801805df786f0ad57a0
Gudelly Goverdhan <gudelligoverdhan9@gmail.com>
2025-11-23 12:08:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navigate between pages
+function navigate(page) {
+  window.location.href = page;
+}
+
+// Display project info (demo)
+function showProjectDetails(name) {
+  alert(`You clicked on: ${name}`);
+}
